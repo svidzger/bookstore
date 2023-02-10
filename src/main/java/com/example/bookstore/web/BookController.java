9369bc8575d6797b9beb47809a01d399529f55e3
svidzger <german.svidzinskij@gmail.com>
2023-02-10 11:32:51 +0200

    C3.2 Tehtäväpalautus

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,16 +1,28 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
 
+	@Autowired
+	BookRepository bookRepository;
+
 	@GetMapping("/index")
-	public String getBooks(Model model) {
+	public String homePage(Model model) {
 		String welcomeMsg = "Hello reading enthusiast!";
 		model.addAttribute("welcomeMsg", welcomeMsg);
 		return "index"; // index.html
+	}
+
+	@GetMapping("/booklist")
+	public String getBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist"; // booklist.html
 	}
 }
